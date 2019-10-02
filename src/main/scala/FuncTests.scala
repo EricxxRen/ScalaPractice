382e34cf11ed541b3f4c3bbdda00de7797460f1e
Eric Ren <ericzjhz@gmail.com>
2019-10-01 23:45:09 -0400

    edit some functions

--- src/main/scala/FuncTests.scala
+++ src/main/scala/FuncTests.scala
@@ -2,6 +2,8 @@ object FuncTests {
   def main(args: Array[String]): Unit = {
     val a = Array(1,2,3,4)
     val a2 = a.map(_ * 2)
-    for (ele <- a2) println(ele)
+//    for (ele <- a2) println(ele)
+    val a3 = a.filter(_ % 2 == 0).map(math.pow(_, 2).toInt)
+    for (ele <- a3) println(ele)
   }
 }
