8206a2e806ae795db65efae9eb6720255782dcdc
Eric Ren <ericzjhz@gmail.com>
2019-10-01 21:28:51 -0400

    first test

--- src/main/scala/FuncTests.scala
+++ src/main/scala/FuncTests.scala
@@ -1,5 +1,7 @@
 object FuncTests {
   def main(args: Array[String]): Unit = {
-    println("Hello World")
+    val a = Array(1,2,3,4)
+    val a2 = a.map(_ * 2)
+    for (ele <- a2) println(ele)
   }
 }
