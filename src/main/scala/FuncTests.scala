69a2d2c5414ca9ebc392e4d6d6a7bd848c7b363f
Eric Ren <ericzjhz@gmail.com>
2019-10-01 23:54:23 -0400

    another expresion

--- src/main/scala/FuncTests.scala
+++ src/main/scala/FuncTests.scala
@@ -4,6 +4,8 @@ object FuncTests {
     val a2 = a.map(_ * 2)
 //    for (ele <- a2) println(ele)
     val a3 = a.filter(_ % 2 == 0).map(math.pow(_, 2).toInt)
-    for (ele <- a3) println(ele)
+    val a4 = (1 to 100).filter((x: Int) => (x % 2 != 0)).map((x: Int) => x * x).reduceLeft(_ + _)
+//    for (ele <- a4) println(ele)
+    println(a4)
   }
 }
